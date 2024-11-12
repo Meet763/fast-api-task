97cf8f76cfc1cd74a87d3a0aaae20f6f52569151
Meet763 <meetkathiriya596@gmail.com>
2024-11-12 20:12:40 +0530

    third commit

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from routes import auth, user
-import os
-import uvicorn
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "welcome to fast api task"}
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(user.router, prefix="/users", tags=["Users"])
 
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Default to 8000 for local development
-    uvicorn.run(app, host="0.0.0.0", port=port)
+
